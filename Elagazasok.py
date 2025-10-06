5ee92849590b9a301a2de95d235625540fd59e07
Geréb Tamás <gereb.tamas@ckik.loc>
2025-10-06 11:30:20 +0200

    orai

---
+++ Elagazasok.py
@@ -0,0 +1,6 @@
+#kerjunk be a felhasznalotol egy szamot es ird ki, hogy paros vagy paratlan
+szam = int(input("Kérek egy számot: "))
+if szam % 2 == 0:
+    print("A szám páros")
+else:
+    print("A szám páratlan")
