52b4aec371e5be911d2be2b8dd584fb02f9e49b6
Geréb Tamás <gereb.tamas@ckik.loc>
2025-11-06 10:31:47 +0100

    m

---
+++ ciklusok.py
@@ -0,0 +1,34 @@
+"""
+Ciklusok - ismetles -loop
+
+Szamlalos - For Ciklus
+Vegigmegy a megadott elemeken vagy intervallumokon
+for elem in range(mettol, meddig, hanyasaval):
+    Ismetlendo folyamat
+
+Tesztelos - While
+
+for karakter in szoveg:
+    Ismetlodo folyamat, szekvencia
+
+"""
+#1 - 10
+for elem in range(1, 11, 1):
+    print(elem, end=" ")
+print()
+
+#elso tiz paros szam 
+for paros in range(0, 20, 2):
+    print(paros, end=" ")
+print()
+
+#szoveg betui koze vesszot
+szoveg = "kapalacs"
+print(szoveg)
+for karakter in szoveg:
+    print(karakter, end=", ")
+print()
+
+
+#hf feladatsorbol aaaaaaaaaaaaa
+#14, 15, 16
