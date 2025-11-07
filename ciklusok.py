0c94848dd48e29f2a0ad95936ce248753a94abb0
Geréb Tamás <gereb.tamas@ckik.loc>
2025-11-07 11:29:47 +0100

    ora

--- ciklusok.py
+++ ciklusok.py
@@ -57,8 +57,9 @@ for tizegy in range(10, 0, -1):
 print()
 
 szovg = "kalapacs"
+n = len(szoveg)
 
-for indx in range(len(szovg)-1, -1, -1):
+for indx in range(len(szovg)-1, -n-1, -1):
     print(szovg[indx],end=" ")
 print()
 
@@ -70,7 +71,6 @@ index = "kalapacs"
 
 for index in range(0, n, 1):
     print(str(index+1)+szoveg[index], end=" ")
-
 print()
 
 # irasson ki 5db veletlen karaktert a szovegbol
@@ -78,5 +78,17 @@ for db in range(0,5,1):
     szam = r.randint(0,n-1)
     print(szoveg[szam],end=" ")
 print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+
+
+print(ujra)
 
 #hf 17-21
+
+#en mar itt elvesztem
---
+++ ujra.py
@@ -0,0 +1,9 @@
+szovg = "kalapacs"
+n = len(szovg)
+ujra = ""
+for indx in range(len(szovg)-1, -1, -1):
+    ujra += szovg[indx]
+    #print(szovg[indx],end=" ")
+print()
+
+print(ujra)
