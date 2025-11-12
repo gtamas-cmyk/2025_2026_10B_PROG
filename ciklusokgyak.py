5b8a52ff01ee02966bca86cfd8b34aaa3c24cb0f
Th3Tamas <tamasgereb450@gmail.com>
2025-11-12 17:28:43 +0100

    HF

--- ciklusokgyak.py
+++ ciklusokgyak.py
@@ -4,12 +4,20 @@ import random as r
 #generaljon ki 10db veletlen szamot
 #irassa ki a szamok oszzeget
 osszeg = 0
+parosdb = 0
+maxsz = 0
 for tiz in range(0, 10, 1):
     velszam = r.randint(1, 9)
     osszeg += velszam
+    if velszam % 2 == 0:
+        parosdb += 1
+    if maxsz == 0 or velszam > maxsz:
+        maxsz = velszam
     print(velszam, end=" ")
 print()
 print("Osszeg:", osszeg)
+print("paros szamok darabja:", parosdb)
+print("legnagyobb veletlen szam:", maxsz)
 
 #hf 
 #hany db paros veletlen szamot generalt ki?
